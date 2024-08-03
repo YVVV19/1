eee99722fbdad98330a80ce8ed18083a604b700c
YVVV19 <vadimyankovskiy2000@gmail.com>
2024-08-03 23:06:53 +0300

    ...

--- main.py
+++ main.py
@@ -1,40 +1,20 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
-# @app.get("/main")
-# def main():
-#     return render_template("index.html")
+@app.get("/")
+def index():
+    return render_template("index.html", menu=MENU)
 
-# @app.get("/menu")
-# def menu():
-#     return render_template("menu.html")
+@app.get("/menu")
+def menu():
+    return render_template("menu.html", menu=MENU)
 
-@app.get("/register")
-def register_get():
-    return render_template("register.html")
-
-@app.post("/register")
-def register_post():
-    nickname = request.form.get('nickname')
-    psd = request.form.get('password')
-    return f"nickname:{nickname}, password:{psd}"
-
-@app.get("/authorization")
-def authorization_get():
-    return render_template("authorization.html")
-
-@app.post("/authorization")
-def authorization_post():
-    message = ''
-    email = request.form.get('email')
-    psd = request.form.get('psd')
-    repeat_psd = request.form.get('repeat_psd')
-    print(psd,repeat_psd)
-    if psd == repeat_psd:
-        message = f"email:{email}, password:{psd}"
-    return message
+MENU = {
+    index.__name__,
+    menu.__name__,
+}
 
 if __name__ == '__main__':
     app.run(
@@ -42,3 +22,4 @@ if __name__ == '__main__':
             port=8080, 
             debug=True,
             )
+    
